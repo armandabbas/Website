cf4cc730700cb40c2204f0298ed63bc865302d60
Armand Abbas <armandabbas@web.de>
2026-02-04 14:20:41 +0100

    Fix build config to include project pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: ('index.html'),
+        project1: ('project-1.html'),
+        project2: ('project-2.html'),
+        project3: ('project-3.html'),
+        project4: ('project-4.html'),
+        project5: ('project-5.html'),
+        project6: ('project-6.html'),
+      },
+    },
+  },
   server: {
     host: true
   }
